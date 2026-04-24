a1b20792f0b34ce4e69b91e0cddab109d1941f63
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-04-23 22:17:37 -0500

    creacion de interfaz y clase Notas

---
+++ Actividad3/Ejercicio_8_2/src/main/java/com/mycompany/ejercicio_8_2/Ejercicio_8_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_8_2;
+
+/**
+ *
+ * @author paula
+ */
+public class Ejercicio_8_2 {
+
+    public static void main(String[] args) {
+/**
+* Método main que sirve de punto de entrada al programa
+*/
+    VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+    principal */
+    miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+    principal */
+    miVentanaPrincipal.setVisible(true); /* Establece la ventana
+    como visible */
+    }
+}
