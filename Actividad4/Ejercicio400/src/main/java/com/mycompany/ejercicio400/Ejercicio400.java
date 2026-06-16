9f1064cc6a2ef02ecb25a31422c0b77fa2936864
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-06-15 21:28:24 -0500

    Ejercicio página 400

---
+++ Actividad4/Ejercicio400/src/main/java/com/mycompany/ejercicio400/Ejercicio400.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio400;
+
+/**
+ *
+ * @author paula
+ */
+public class Ejercicio400 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
