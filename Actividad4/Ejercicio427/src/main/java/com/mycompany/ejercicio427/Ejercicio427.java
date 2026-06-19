4cbcea7179ef225b3757cca526828d1ea6aac4fc
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-06-18 21:30:32 -0500

    Ejercicio página 427

---
+++ Actividad4/Ejercicio427/src/main/java/com/mycompany/ejercicio427/Ejercicio427.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio427;
+
+/**
+ *
+ * @author paula
+ */
+public class Ejercicio427 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        miVentana.setVisible(true);
+        miVentana.setResizable(false);
+    }
+}
