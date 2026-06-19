26fa7422bb3855622343b361f606cccfa09a440d
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-06-19 15:50:50 -0500

    Creacion de actividad5

---
+++ Actividad5/AplicacionInterfaz/src/main/java/com/mycompany/aplicacioninterfaz/AplicacionInterfaz.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplicacioninterfaz;
+
+/**
+ *
+ * @author paula
+ */
+public class AplicacionInterfaz {
+
+    public static void main(String[] args) {
+        Formulario miVentana = new Formulario();
+        miVentana.setVisible(true);
+        miVentana.setResizable(false);
+        
+    }
+}
