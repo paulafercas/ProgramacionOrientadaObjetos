e329527ff03294ad93204032c463615436e8fa7b
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-06-29 16:43:40 -0500

    Ejercicio 4.4 completado

---
+++ Actividad6/Ejercicio4_4/src/main/java/com/mycompany/ejercicio4_4/Ejercicio4_4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4_4;
+
+/**
+ *
+ * @author paula
+ */
+public class Ejercicio4_4 {
+
+    public static void main(String[] args) {
+        
+        Profesor profesor1 = new ProfesorTitular();
+        profesor1.imprimir();
+    }
+}
---
+++ Actividad6/Ejercicio4_4/src/main/java/com/mycompany/ejercicio4_4/ProfesorTitular.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio4_4;
+
+/**
+ *
+ * @author paula
+ */
+public class ProfesorTitular extends Profesor{
+    
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+    
+}
